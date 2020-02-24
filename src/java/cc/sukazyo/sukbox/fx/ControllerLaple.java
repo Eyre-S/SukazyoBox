332bde5a8a3c5dc1130b849f0d360e8223c89192
Eyre_S <SteveW2004@outlook.com>
2020-02-24 13:28:27 +0800

    入口制作完毕
     - 添加了 key = 23333 作为验证登录入口
     - 添加了 Kill Lapis_Apple 窗体

---
+++ src/java/cc/sukazyo/sukbox/fx/ControllerLaple.java
@@ -0,0 +1,32 @@
+package cc.sukazyo.sukbox.fx;
+
+import cc.sukazyo.sukbox.util.Logout;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class ControllerLaple {
+	
+	public Stage mainStage;
+	private AnchorPane root;
+	
+	public void load () {
+		
+		/** 加载fxml */
+		root = Util.getFxml("laple.fxml");
+		mainStage.setScene(new Scene(root));
+		mainStage.initStyle(StageStyle.TRANSPARENT);
+		mainStage.show();
+		
+	}
+	
+	@FXML
+	private void actionButtonExit () {
+		Logout.info("退出应用程序");
+		Platform.exit();
+	}
+	
+}
--- src/java/cc/sukazyo/sukbox/fx/ControllerLogin.java
+++ src/java/cc/sukazyo/sukbox/fx/ControllerLogin.java
@@ -19,21 +19,28 @@ public class ControllerLogin {
 		String key = keyInput.getText();
 		Logout.info("url=https://srv.sukazyo.cc/suk-box/configure/" + "; key=" + key + "; 正在验证...");
 		String re = Network.sendGet("https://srv.sukazyo.cc/suk-box/configure/", "key=" + key);
-		if (re.equals("false")) {
+		Logout.info("返回的是：" + re);
+		if (re.equals("N/A")) {
 			Logout.info("密钥错误");
 			Util.infoWarnError (
 					"密钥错误",
 					"密钥没有通过验证",
-					"请检查您的输入是否正确");
-		} else if (re.equals("true")) {
+					"输错了吧，再试试\n顺带一提，验证链接是 https://srv.sukazyo.cc/suk-box/configure/ 哦(尝试暗示)");
+		} else if (re.equals("Eyre_S")) {
 			Logout.info("密钥正确，尝试跳转页面");
 			/* 启动main */
 			HomeDesktop.start();
 			Logout.info("退出welcome面板");
 			Stage curSt = (Stage)keyInput.getScene().getWindow();
 			curSt.close();
+		} else if (re.equals("Kill-LAPPLE")) {
+			Logout.info("确认Kill-LAPLE命令，尝试执行");
+			HomeLaple.start();
+			Logout.info("退出welcome面板");
+			Stage curSt = (Stage)keyInput.getScene().getWindow();
+			curSt.close();
 		} else {
-			Util.infoWarnError("网络错误", "无法连接到网络...\n请稍后再试");
+			Util.infoWarnError("网络错误", "好像无法连接到网络讷=-=\n请稍后再试");
 		}
 	}
 	
