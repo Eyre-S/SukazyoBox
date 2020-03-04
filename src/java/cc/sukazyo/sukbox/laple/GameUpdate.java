0d0e1b39d9708f59552a11af1a797391f0435229
Eyre_S <SteveW2004@outlook.com>
2020-03-04 11:46:40 +0800

    游戏Update线程维护
     · 增加点击后的数据平滑更新效果
     · 通过休眠10ms来优化CPU占用
        · 在过于卡顿(一次更新进行20ms以上)时关闭平滑更新

--- src/java/cc/sukazyo/sukbox/laple/GameUpdate.java
+++ src/java/cc/sukazyo/sukbox/laple/GameUpdate.java
@@ -2,7 +2,9 @@ package cc.sukazyo.sukbox.laple;
 
 
 import cc.sukazyo.sukbox.util.Logout;
+import sun.rmi.runtime.Log;
 
+import java.text.DecimalFormat;
 import java.util.Date;
 
 public class GameUpdate extends Thread {
@@ -16,13 +18,15 @@ public class GameUpdate extends Thread {
 		long now;
 		double speed = 0;
 		
+		DecimalFormat df = new DecimalFormat( "0.0000000000");
+		
 		while (true) {
 			
 			// 时间戳操作
 			now = new Date().getTime();
 			speed = (now - last) / 60000.0;
 			last = new Date().getTime();
-//			Logout.info("当前时间戳" + String.valueOf(now));
+//			Logout.info("当前时间戳 : " + now + " ;更新倍率 : " + df.format(speed));
 			
 			// 数据复制
 			double health       = GameInfo.laple.health.get();
@@ -64,22 +68,22 @@ public class GameUpdate extends Thread {
 			upower += 10;
 			
 			// 应用更改数值
-			health      += uhealth      * speed + GameInfo.laple.uhealth     ;
-			GameInfo.laple.uhealth      = 0;
-			temperature += utemperature * speed + GameInfo.laple.utemperature;
-			GameInfo.laple.utemperature = 0;
-			bloody      += ubloody      * speed + GameInfo.laple.ubloody     ;
-			GameInfo.laple.ubloody      = 0;
-			hunger      += uhunger      * speed + GameInfo.laple.uhunger     ;
-			GameInfo.laple.uhunger      = 0;
-			water       += uwater       * speed + GameInfo.laple.uwater      ;
-			GameInfo.laple.uwater       = 0;
-			clean       += uclean       * speed + GameInfo.laple.uclean      ;
-			GameInfo.laple.uclean       = 0;
-			power       += upower       * speed + GameInfo.laple.upower      ;
-			GameInfo.laple.upower       = 0;
-			mood        += umood        * speed + GameInfo.laple.umood       ;
-			GameInfo.laple.umood        = 0;
+			health      += uhealth      * speed + (GameInfo.laple.uhealth      > 0 ? (Math.min(GameInfo.laple.uhealth     , (60 * speed))) : Math.max(GameInfo.laple.uhealth     , (-60 * speed)));
+			GameInfo.laple.uhealth      = GameInfo.laple.uhealth      > 0 ? (GameInfo.laple.uhealth      < (60 * speed) ? 0 : GameInfo.laple.uhealth      - (60 * speed)) : (GameInfo.laple.uhealth      > (-60 * speed) ? 0 : GameInfo.laple.uhealth      - (-60 * speed));
+			temperature += utemperature * speed + (GameInfo.laple.utemperature > 0 ? (Math.min(GameInfo.laple.utemperature, (300 * speed))) : Math.max(GameInfo.laple.utemperature, (-300 * speed)));
+			GameInfo.laple.utemperature = GameInfo.laple.utemperature > 0 ? (GameInfo.laple.utemperature < (300 * speed) ? 0 : GameInfo.laple.utemperature - (300 * speed)) : (GameInfo.laple.utemperature > (-300 * speed) ? 0 : GameInfo.laple.utemperature - (-300 * speed));
+			bloody      += ubloody      * speed + (GameInfo.laple.ubloody      > 0 ? (Math.min(GameInfo.laple.ubloody     , (60 * speed))) : Math.max(GameInfo.laple.ubloody     , (-60 * speed)));
+			GameInfo.laple.ubloody      = GameInfo.laple.ubloody      > 0 ? (GameInfo.laple.ubloody      < (60 * speed) ? 0 : GameInfo.laple.ubloody      - (60 * speed)) : (GameInfo.laple.ubloody      > (-60 * speed) ? 0 : GameInfo.laple.ubloody      - (-60 * speed));
+			hunger      += uhunger      * speed + (GameInfo.laple.uhunger      > 0 ? (Math.min(GameInfo.laple.uhunger     , (60 * speed))) : Math.max(GameInfo.laple.uhunger     , (-60 * speed)));
+			GameInfo.laple.uhunger      = GameInfo.laple.uhunger      > 0 ? (GameInfo.laple.uhunger      < (60 * speed) ? 0 : GameInfo.laple.uhunger      - (60 * speed)) : (GameInfo.laple.uhunger      > (-60 * speed) ? 0 : GameInfo.laple.uhunger      - (-60 * speed));
+			water       += uwater       * speed + (GameInfo.laple.uwater       > 0 ? (Math.min(GameInfo.laple.uwater      , (60 * speed))) : Math.max(GameInfo.laple.uwater      , (-60 * speed)));
+			GameInfo.laple.uwater       = GameInfo.laple.uwater       > 0 ? (GameInfo.laple.uwater       < (60 * speed) ? 0 : GameInfo.laple.uwater       - (60 * speed)) : (GameInfo.laple.uwater       > (-60 * speed) ? 0 : GameInfo.laple.uwater       - (-60 * speed));
+			clean       += uclean       * speed + (GameInfo.laple.uclean       > 0 ? (Math.min(GameInfo.laple.uclean      , (60 * speed))) : Math.max(GameInfo.laple.uclean      , (-60 * speed)));
+			GameInfo.laple.uclean       = GameInfo.laple.uclean       > 0 ? (GameInfo.laple.uclean       < (60 * speed) ? 0 : GameInfo.laple.uclean       - (60 * speed)) : (GameInfo.laple.uclean       > (-60 * speed) ? 0 : GameInfo.laple.uclean       - (-60 * speed));
+			power       += upower       * speed + (GameInfo.laple.upower       > 0 ? (Math.min(GameInfo.laple.upower      , (60 * speed))) : Math.max(GameInfo.laple.upower      , (-60 * speed)));
+			GameInfo.laple.upower       = GameInfo.laple.upower       > 0 ? (GameInfo.laple.upower       < (60 * speed) ? 0 : GameInfo.laple.upower       - (60 * speed)) : (GameInfo.laple.upower       > (-60 * speed) ? 0 : GameInfo.laple.upower       - (-60 * speed));
+			mood        += umood        * speed + (GameInfo.laple.umood        > 0 ? (Math.min(GameInfo.laple.umood       , (60 * speed))) : Math.max(GameInfo.laple.umood       , (-60 * speed)));
+			GameInfo.laple.umood        = GameInfo.laple.umood        > 0 ? (GameInfo.laple.umood        < (60 * speed) ? 0 : GameInfo.laple.umood        - (60 * speed)) : (GameInfo.laple.umood        > (-60 * speed) ? 0 : GameInfo.laple.umood        - (-60 * speed));
 			
 			// 数据容错
 			health       = health      > 1 ? 1 : health      ;
@@ -109,6 +113,17 @@ public class GameUpdate extends Thread {
 			GameInfo.laple.clean      .set(clean      );
 			GameInfo.laple.power      .set(power      );
 			GameInfo.laple.mood       .set(mood       );
+			
+			// CPU占用维护
+			try {
+				if (speed < (20 / 60000)) {
+//					Logout.info("系统卡顿，解除占用维护");
+				} else {
+					Thread.sleep(10);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			
 		}
 		
